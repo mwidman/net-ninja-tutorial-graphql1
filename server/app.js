f9e183f32528f6961a12cb4ecf224cebd4449e8b
mwidman <2118673+mwidman@users.noreply.github.com>
2019-06-24 20:34:15 -0700

    Added connection to MongoDB.

--- server/app.js
+++ server/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const graphqlHttp = require('express-graphql');
+const mongoose = require('mongoose');
 
 const schema = require('./schema/schema');
 
 const app = express();
+
+mongoose.connect('<connection-string>');
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+})
 
 const port = 5000;
 
