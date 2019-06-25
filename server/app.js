89472b5a4d318774f66cc8b9a55990e60ba65314
mwidman <2118673+mwidman@users.noreply.github.com>
2019-06-24 17:22:44 -0700

    Created basic Book schema.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const graphqlHttp = require('express-graphql');
+
+const schema = require('./schema/schema');
 
 const app = express();
 
 const port = 5000;
+
+app.use('/graphql', graphqlHttp({
+  schema
+}));
 
 app.listen(port, () => {
   console.log(`now listening for requests on port ${port}`);
