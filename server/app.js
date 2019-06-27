0ae8f753b133aa4575c8298d9f7e37fd91f0a3f4
mwidman <2118673+mwidman@users.noreply.github.com>
2019-06-26 18:13:57 -0700

    Added cors middleware to server to allow connections from other locations.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const graphqlHttp = require('express-graphql');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const schema = require('./schema/schema');
 const { connectionString } = require('./env');
@@ -14,7 +15,7 @@ mongoose.connection.once('open', () => {
 
 const port = 5000;
 
-app.use('/graphql', graphqlHttp({
+app.use('/graphql', cors(), graphqlHttp({
   schema,
   graphiql: true
 }));
